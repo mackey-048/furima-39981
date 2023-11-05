e4607c5dd6f88887820176f4af4f59d6d8fa8a19
mackey-048 <yokote.shoma.4488@gmail.com>
2023-11-06 00:10:51 +0900

    validatesの修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,6 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :nickname, :first_name, :last_name, :first_name_kana, :last_name_kana, :birthday, presence: true
-  validates :first_name_kana, :last_name_kana, format: { with: /[\p{Katakana} ー－]+/, message: "はカタカナで入力してください" }
+  validates :first_name_kana, :last_name_kana, format: { with: /\A[\p{katakana}ー]+\z/, message: "はカタカナで入力してください" }
   validates :first_name, :last_name, format: { with: /\A[ぁ-んァ-ヶ一-龠々]+\z/, message: "は漢字、ひらがな、カタカナのみを含めてください" }
 end
