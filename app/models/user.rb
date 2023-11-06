5bcaff26bb07ea74ae0a834367f10970fdf744ad
mackey-048 <yokote.shoma.4488@gmail.com>
2023-11-06 14:26:20 +0900

    validates長音記号の追記

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,5 +8,5 @@ class User < ApplicationRecord
   PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
   validates_format_of :password, with: PASSWORD_REGEX, message: 'は英字と数字の両方を含めて設定してください'
   validates :first_name_kana, :last_name_kana, format: { with: /\A[\p{katakana}ー]+\z/, message: "はカタカナで入力してください" }
-  validates :first_name, :last_name, format: { with: /\A[ぁ-んァ-ヶ一-龠々]+\z/, message: "は漢字、ひらがな、カタカナのみを含めてください" }
+  validates :first_name, :last_name, format: { with: /\A[ぁ-んァ-ヶー一-龠々ー]+\z/, message: "は漢字、ひらがな、カタカナのみを含めてください" }
 end
