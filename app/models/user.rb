f859056648f7ac6e7ce9198d5a0bf557c3838f4d
mackey-048 <yokote.shoma.4488@gmail.com>
2023-11-07 01:40:00 +0900

    itemsモデルバリデーション追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,4 +9,5 @@ class User < ApplicationRecord
   validates_format_of :password, with: PASSWORD_REGEX, message: 'は英字と数字の両方を含めて設定してください'
   validates :first_name_kana, :last_name_kana, format: { with: /\A[\p{katakana}ー]+\z/, message: "はカタカナで入力してください" }
   validates :first_name, :last_name, format: { with: /\A[ぁ-んァ-ヶー一-龠々ー]+\z/, message: "は漢字、ひらがな、カタカナのみを含めてください" }
+  has_many :items
 end
