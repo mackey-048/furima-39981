5129561a7280e55e2f488d60e14a2433ff58daa6
mackey-048 <yokote.shoma.4488@gmail.com>
2023-11-06 11:08:07 +0900

    validatesの追記（パスワードが英数字）

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,8 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :nickname, :first_name, :last_name, :first_name_kana, :last_name_kana, :birthday, presence: true
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+  validates_format_of :password, with: PASSWORD_REGEX, message: 'は英字と数字の両方を含めて設定してください'
   validates :first_name_kana, :last_name_kana, format: { with: /\A[\p{katakana}ー]+\z/, message: "はカタカナで入力してください" }
   validates :first_name, :last_name, format: { with: /\A[ぁ-んァ-ヶ一-龠々]+\z/, message: "は漢字、ひらがな、カタカナのみを含めてください" }
 end
