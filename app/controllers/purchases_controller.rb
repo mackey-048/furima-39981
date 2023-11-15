3aa5b3743c5cd1157b41004a23a1c6e57426a9ac
mackey-048 <yokote.shoma.4488@gmail.com>
2023-11-15 13:41:11 +0900

    フォームオブジェクトの実装

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -2,14 +2,28 @@ class PurchasesController < ApplicationController
   before_action :authenticate_user!, only: [:index]
 
   def index
+    @item = Item.find(params[:item_id])
+    @purchase_order = PurchaseOrder.new
   end
 
+  def new
+    @purchase_order = PurchaseOrder.new
+  end
+
+  def create
+    @item = Item.find(params[:item_id])
+    @purchase_order = PurchaseOrder.new(purchase_params)
+    if @purchase_order.valid?
+      @purchase_order.save
+      redirect_to root_path
+    else
+      render :index, status: :unprocessable_entity
+  end
+end
+  
   private
 
   def purchase_params
-    params.permit(:purchase).merge(user_id: current_user.id)
-  end
-
-  def order
+    params.require(:purchase_order).permit(:postcode, :prefecture_id, :city, :block, :building, :phone_number).merge(user_id: current_user.id, item_id: @item.id)
   end
 end
--- app/models/purchase_order.rb
+++ app/models/purchase_order.rb
@@ -1,17 +1,18 @@
 class PurchaseOrder
 
   include ActiveModel::Model
-  attr_accessor :postcode, :prefecture_id, :city, :block, :building, :phone_number, :user_id
+  attr_accessor :postcode, :prefecture_id, :city, :block, :building, :phone_number, :user_id, :item_id
 
   with_options presence: true do
-    validates :price, numericality: {only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999, message: 'is invalid'}
-    validates :user_id
+    validates :city, :block
+    validates :user_id, :item_id
     validates :postcode, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
+    validates :phone_number, numericality: { only_integer: true, greater_than_or_equal_to: 10_000_000_00, less_than: 1_000_000_000_00 }
   end
   validates :prefecture_id, numericality: {other_than: 0, message: "can't be blank"}
 
   def save
     purchase = Purchase.create(item_id: item_id, user_id: user_id)
-    Order.create(postcode: postcode, prefecture_id: prefecture_id, city: city, block: block, building: building, phone_number: phone_number, purchase: purchase.id)
+    Order.create(postcode: postcode, prefecture_id: prefecture_id, city: city, block: block, building: building, phone_number: phone_number, purchase_id: purchase.id)
   end
 end
