f9bfe4987129a8345ed709e33d4dfafb717b9c9c
mackey-048 <yokote.shoma.4488@gmail.com>
2023-11-16 22:02:43 +0900

    バリデーションの修正

--- app/models/purchase_order.rb
+++ app/models/purchase_order.rb
@@ -7,7 +7,7 @@ class PurchaseOrder
     validates :city, :block
     validates :user_id, :item_id
     validates :postcode, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
-    validates :phone_number, presence: true, format: { with: /\A\d{10,11}\z/, message: "は10桁または11桁の数字で入力してください" }
+    validates :phone_number, format: { with: /\A\d{10,11}\z/, message: "は10桁または11桁の数字で入力してください" }
     validates :token
   end
   validates :prefecture_id, numericality: {other_than: 0, message: "can't be blank"}
