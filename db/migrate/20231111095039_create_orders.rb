38761b87eadc3354850108e0e54f0f3a2481ebb4
mackey-048 <yokote.shoma.4488@gmail.com>
2023-11-17 14:31:57 +0900

    oederテーブルの修正

--- db/migrate/20231111095039_create_orders.rb
+++ db/migrate/20231111095039_create_orders.rb
@@ -5,7 +5,7 @@ class CreateOrders < ActiveRecord::Migration[7.0]
       t.integer    :prefecture_id,  null: false
       t.string     :city,           null: false
       t.string     :block,          null: false
-      t.string     :building,       
+      t.string     :building       
       t.string     :phone_number,   null: false
       t.references :purchase,       null: false, foreign_key: true
 
